257076e210bae56f716c1e5600ce94243bd65f6e
Kushagra Ankit <Kushagraankit05@gmail.com>
2024-09-16 02:57:18 +0530

    Update data.js

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -89,6 +89,96 @@ const data = [
       position: "Web Designer",
       organization: "XYZ Organisation"
     },
+    {
+      id: 16,
+      name: "Eleanor Pena",
+      position: "President of Sales",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 17,
+      name: "Esther Howard",
+      position: "Marketing Coordinator",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 18,
+      name: "Albert Flores",
+      position: "Nursing Assistant",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 19,
+      name: "John Doe",
+      position: "Marketing Head",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 20,
+      name: "Savannah Nguyen",
+      position: "Web Designer",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 21,
+      name: "Eleanor Pena",
+      position: "President of Sales",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 22,
+      name: "Esther Howard",
+      position: "Marketing Coordinator",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 23,
+      name: "Albert Flores",
+      position: "Nursing Assistant",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 24,
+      name: "John Doe",
+      position: "Marketing Head",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 25,
+      name: "Savannah Nguyen",
+      position: "Web Designer",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 26,
+      name: "Eleanor Pena",
+      position: "President of Sales",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 27,
+      name: "Esther Howard",
+      position: "Marketing Coordinator",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 28,
+      name: "Albert Flores",
+      position: "Nursing Assistant",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 29,
+      name: "John Doe",
+      position: "Marketing Head",
+      organization: "XYZ Organisation"
+    },
+    {
+      id: 30,
+      name: "Savannah Nguyen",
+      position: "Web Designer",
+      organization: "XYZ Organisation"
+    },
     
     
   ];
